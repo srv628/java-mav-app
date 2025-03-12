33db1c45d747ad0446d57bd96e37bf4b8b426c3b
sourabh nagve <srv628@gmail.com>
2025-03-12 06:39:45 +0000

    Edit script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,19 @@
-def buildApp(){
-    echo "building the code"
-}
-def testApp(){
-    echo "testing the code"
-}
-def deployApp(){
-    echo "deploying the code"
-    echo " deploying versio ${params.VERSION}"
-}
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
 return this
