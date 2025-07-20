232f2bdd185e8ef1d35dcb41472d796d6507311c
Sourabh Nagve <90417650+srv628@users.noreply.github.com>
2025-07-20 14:46:38 +0530

    Update scriptdemo.groovy

--- scriptdemo.groovy
+++ scriptdemo.groovy
@@ -6,7 +6,7 @@ def buildJar(){
 def buildImage(){
       echo "building the docker image"
                     withCredentials([usernamePassword(credentialsId:"dockerHub",usernameVariable:"USER",passwordVariable:"PASS")]){
-                    sh " docker build -t srvwin/dockerinitial:javamapp-2.0 . "
+                    sh "docker build -t srvwin/dockerinitial:javamapp-2.0 ."
 
                     sh "echo $PASS | docker login -u $USER --password-stdin"
                 
